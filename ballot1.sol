a733611131b8bfc4a9a485edd4c43c0b482c90b6
Vuk Djukic <vukdukic@gmail.com>
2018-07-04 12:52:41 -0700

    Update ballot1.sol

--- ballot1.sol
+++ ballot1.sol
@@ -10,9 +10,16 @@ contract Campaign {
     }
 
     // variables
+    Request[] public requests;
     address public manager;
     uint public minimumContribution;
     address[] public approvers;
+
+    // modifier
+    modifier restricted() {
+        require(msg.sender == manager);
+        _; //place where the function will be pasted
+    }
 
     // constructor function
     function Campaign(uint minimum) public {
@@ -24,5 +31,16 @@ contract Campaign {
         require(msg.value > minimumContribution);
 
         approvers.push(msg.sender);
+    }
+
+    function createRequest(string description, uint value, address recipient) public restricted {
+        Request newRequest = Request({
+            description: description, 
+            value: value,
+            recipient: recipient,
+            complete: false
+        });
+
+        request.push(newRequest);
     }
 }
