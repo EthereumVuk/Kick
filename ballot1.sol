bedaba89b14743140d0c4f6844d8d23f3e642807
Vuk Djukic <vukdukic@gmail.com>
2018-07-05 08:24:29 -0700

    Update ballot1.sol

--- ballot1.sol
+++ ballot1.sol
@@ -1,5 +1,18 @@
 pragma solidity ^0.4.24;
 
+contract CampaignFactory {
+    address[] public deployedCampaigns;
+
+    function createCampaign(uint minimum) public {
+        address newCampaign = new Campaign(minimum, msg.sender); // constructor argument
+        deployedCampaign.push(newCampaign);
+    }
+
+    function getDeployedCampaigns() public view returns (address[]) { // public means that anyone can call this function
+        return deployedCampaigns;                                     // view means that no data can be modified
+    }                                                                 // return value of type address array
+}
+
 contract Campaign {
     //struct
     struct Request {
@@ -16,6 +29,7 @@ contract Campaign {
     address public manager;
     uint public minimumContribution;
     mapping(address => bool) public approvers;
+    uint public approversCount;
 
     // modifier
     modifier restricted() {
@@ -24,8 +38,8 @@ contract Campaign {
     }
 
     // constructor function
-    function Campaign(uint minimum) public {
-        manager = msg.sender;
+    function Campaign(uint minimum, address creator) public {
+        manager = creator;
         minimumContribution = minimum;
     }
 
@@ -33,6 +47,7 @@ contract Campaign {
         require(msg.value > minimumContribution);
 
         approvers[msg.sender] = true;
+        approversCount++;
     }
 
     function createRequest(string description, uint value, address recipient) public restricted {
@@ -47,7 +62,7 @@ contract Campaign {
         request.push(newRequest);
     }
 
-    function approveRequest(uint index) {
+    function approveRequest(uint index) public {
         Request storage request = requests[index];
 
         require(approvers[msg.sender]);
@@ -61,8 +76,10 @@ contract Campaign {
         // capital R specifies that we are creating a variable that is going to refer to the Request struct
         Request storage request = requests[index];
         
+        require(request.approvalCount > (approversCount / 2)); // number of approvals vs number of people that contributed to the campaign
         require(!request.complete);
 
+        request.recipient.transfer(request.value);
         request.complete = true;
     }
 }
