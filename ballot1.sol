ed55db1c2aa1283121f331b4459e9be9e4e12a8e
Vuk Djukic <vukdukic@gmail.com>
2018-07-07 07:45:09 -0700

    Update ballot1.sol

--- ballot1.sol
+++ ballot1.sol
@@ -1,17 +1,4 @@
-pragma solidity ^0.4.24;
-
-contract CampaignFactory {
-    address[] public deployedCampaigns;
-
-    function createCampaign(uint minimum) public {
-        address newCampaign = new Campaign(minimum, msg.sender); // constructor argument
-        deployedCampaign.push(newCampaign);
-    }
-
-    function getDeployedCampaigns() public view returns (address[]) { // public means that anyone can call this function
-        return deployedCampaigns;                                     // view means that no data can be modified
-    }                                                                 // return value of type address array
-}
+pragma solidity ^0.4.21;
 
 contract Campaign {
     //struct
@@ -20,16 +7,13 @@ contract Campaign {
         uint value;
         address recipient;
         bool complete;
-        uint approvalCount;
-        mapping(address => bool) approvals;
     }
 
     // variables
     Request[] public requests;
     address public manager;
     uint public minimumContribution;
-    mapping(address => bool) public approvers;
-    uint public approversCount;
+    address[] public approvers;
 
     // modifier
     modifier restricted() {
@@ -38,16 +22,15 @@ contract Campaign {
     }
 
     // constructor function
-    function Campaign(uint minimum, address creator) public {
-        manager = creator;
+    function Campaign(uint minimum) public {
+        manager = msg.sender;
         minimumContribution = minimum;
     }
 
     function contribute() public payable {
         require(msg.value > minimumContribution);
 
-        approvers[msg.sender] = true;
-        approversCount++;
+        approvers.push(msg.sender);
     }
 
     function createRequest(string description, uint value, address recipient) public restricted {
@@ -55,31 +38,9 @@ contract Campaign {
             description: description, 
             value: value,
             recipient: recipient,
-            complete: false,
-            approvalCount: 0
+            complete: false
         });
 
         request.push(newRequest);
-    }
-
-    function approveRequest(uint index) public {
-        Request storage request = requests[index];
-
-        require(approvers[msg.sender]);
-        require(!request.approvals[msg.sender]);
-
-        request.approvals[msg.sender] = true;
-        request.approvalCount++;
-    }
-
-    function finalizeRequest(uint index) public restricted {
-        // capital R specifies that we are creating a variable that is going to refer to the Request struct
-        Request storage request = requests[index];
-        
-        require(request.approvalCount > (approversCount / 2)); // number of approvals vs number of people that contributed to the campaign
-        require(!request.complete);
-
-        request.recipient.transfer(request.value);
-        request.complete = true;
     }
 }
