d965b29f6ad1a802657d719f5d7f20ccaaf4c2b2
Vuk Djukic <vukdukic@gmail.com>
2018-08-02 21:25:16 -0700

    Create index2.js

---
+++ index2.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { Card, Button } from 'semantic-ui-react';
+import factory from '../ethereum/factory';
+import Layout from '../components/Layout';
+
+class CampaignIndex extends Component {
+  static async getInitialProps() {
+    const campaigns = await factory.methods.getDeployedCampaigns().call();
+
+    return { campaigns };
+  }
+
+  renderCampaigns() {
+    const items = this.props.campaigns.map(address => {
+      return {
+        header: address,
+        description: (
+          <Link route={`/campaigns/${address}`}>
+            <a>View Campaign</a>
+          </Link>
+        ),
+        fluid: true
+      };
+    });
+
+    return <Card.Group items={items} />;
+  }
+
+  render() {
+    return (
+      <Layout>
+        <div>
+          <h1>Open Campaigns</h1>
+
+          <Link route="/campaigns/new">
+            <a>
+              <Button
+                floated="right"
+                content="Create Campaign"
+                icon="add circle"
+                primary
+              />
+            </a>
+          </Link>
+
+          {this.renderCampaigns()}
+        </div>
+      </Layout>
+    );
+  }
+}
+
+export default CampaignIndex;
