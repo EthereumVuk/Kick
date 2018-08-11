fe2f7fdc0b3c39e521d2d6d232efb9680bdde611
Vuk Djukic <vukdukic@gmail.com>
2018-08-11 08:34:06 -0700

    Create new.v1.js

---
+++ pages/campaigns/requests/new.v1.js
@@ -0,0 +1,71 @@
+import React, { Component } from 'react'
+import { Form, Button, Message, Input } from 'semantic-ui-react'
+import Campaign from '../../../ethereum/campaign'
+import web3 from '../../../ethereum/web3' 
+import { Link, Router } from '../../../routes'
+import Layout from '../../../components/Layout'
+
+class RequestNew extends Component {
+    state = {
+        value: '',
+        description: '',
+        recipient: ''
+    }
+
+    static async getInitialProps(props) {
+        const { address } = props.query
+
+        return { address }
+    }
+
+    onSubmit = async event => {
+        event.preventDefault()
+
+        const campaign = Campaign(this.props.address)
+        const { description, value, recipient } = this.state
+
+        try {
+            const accounts = await web3.eth.getAccounts()
+            await campaign.methods.createRequest()
+        } catch (err) {
+
+        }
+    }
+
+    render() {
+        return (
+            <Layout>
+            <h3>Create a Request</h3>
+            <Form>
+                <Form.Field>
+                    <label>Description</label>
+                    <Input 
+                        value={this.state.description}
+                        onChange={ event => this.setState({ description: event.target.value }) }
+                    />
+                </Form.Field>
+
+                <Form.Field>
+                    <label>Value in Ether</label>
+                    <Input 
+                        value={this.state.value}
+                        onChange={ event => this.setState({ value: event.target.value }) }
+                    />
+                </Form.Field>
+
+                <Form.Field>
+                    <label>Recipient</label>
+                    <Input 
+                        value={this.state.recipient}
+                        onChange={ event => this.setState({ recipient: event.target.value }) }
+                    />
+               </Form.Field>
+
+               <Button primary>Create</Button>
+            </Form>
+            </Layout>
+        )
+    }
+}
+
+export default RequestNew
